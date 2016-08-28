5ec4053b92f36b1aa18052ae533ec461f45fcfef
Ravi Shankar Jha <ravi.sun66@gmail.com>
2016-08-28 20:35:20 +0530

    traingle project done

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,13 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == b and b == c
+  	:equilateral
+  elsif a == b or b == c or c == a
+  	:isosceles
+  elsif a != b and b!=c and c!=a
+  	:scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
